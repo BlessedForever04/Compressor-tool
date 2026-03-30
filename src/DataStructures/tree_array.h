45af5e62abbc5937f679645b92e7368ddc0472b2
BlessForever04 <vinitwankhade411@gmail.com>
2026-03-30 20:31:08 +0530

    I dont know why

--- src/DataStructures/tree_array.h
+++ src/DataStructures/tree_array.h
@@ -20,31 +20,6 @@ void swapData(uint8_t *a, uint8_t *b) {
     *b = temp;
 }
 
-int partition(struct Tree_array *tree_array, int low, int high) {
-    int pivot = tree_array->node[low].pair.frequency;
-    int i = low + 1;
-    int j = high;
-
-    while (1) {
-        while (i <= high && tree_array->node[i].pair.frequency <= pivot) i++;
-        while (tree_array->node[j].pair.frequency > pivot) j--;
-        if (i >= j) break;
-        swapFrequency(&tree_array->node[i].pair.frequency, &tree_array->node[j].pair.frequency);
-        swapData(&tree_array->node[i].pair.data, &tree_array->node[j].pair.data);
-    }
-    swapFrequency(&tree_array->node[low].pair.frequency, &tree_array->node[j].pair.frequency);
-    swapData(&tree_array->node[low].pair.data, &tree_array->node[j].pair.data);
-    return j;
-}
-
-void quickSort(struct Tree_array *tree_array, int low, int high) {
-    if (low < high) {
-        int p = partition(tree_array, low, high);
-        quickSort(tree_array, low, p - 1); 
-        quickSort(tree_array, p + 1, high);
-    }
-}
-
 void merge(){
 
 }
@@ -85,6 +60,33 @@ void reverse(struct Tree_array *tree_array){
         j--;
     }
 }
+
+
+int partition(struct Tree_array *tree_array, int low, int high) {
+    int pivot = tree_array->node[low].pair.frequency;
+    int i = low + 1;
+    int j = high;
+
+    while (1) {
+        while (i <= high && tree_array->node[i].pair.frequency <= pivot) i++;
+        while (tree_array->node[j].pair.frequency > pivot) j--;
+        if (i >= j) break;
+        swapFrequency(&tree_array->node[i].pair.frequency, &tree_array->node[j].pair.frequency);
+        swapData(&tree_array->node[i].pair.data, &tree_array->node[j].pair.data);
+    }
+    swapFrequency(&tree_array->node[low].pair.frequency, &tree_array->node[j].pair.frequency);
+    swapData(&tree_array->node[low].pair.data, &tree_array->node[j].pair.data);
+    return j;
+}
+
+void quickSort(struct Tree_array *tree_array, int low, int high) {
+    if (low < high) {
+        int p = partition(tree_array, low, high);
+        quickSort(tree_array, low, p - 1); 
+        quickSort(tree_array, p + 1, high);
+    }
+}
+
 void buildTree(struct Tree_array *tree_array){
     printf("Before:\n");
     tempPrint(*tree_array);
@@ -92,4 +94,5 @@ void buildTree(struct Tree_array *tree_array){
     reverse(tree_array);
     printf("\nAfter:\n");
     tempPrint(*tree_array);
+    
 }
